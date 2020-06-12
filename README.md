e32c1ddc4072eaa89f7b591eef2add4b3433a237
Akshat Agarwal <humancalico@disroot.org>
2020-06-12 20:05:43 +0530

    lesse

---
+++ README.md
@@ -0,0 +1,3 @@
+Dependencies:
+    openssl-devel for Fedora
+    libssl-dev on Ubuntu
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
-use deno_core::plugin_api::*;
+use super::dispatch_json::{Deserialize, JsonOp, Value};
+use crate::op_error::OpError;
+use crate::state::State;
+use deno_core::CoreIsolate;
+use deno_core::ZeroCopyBuf;
+use std::collections::HashMap;
+use std::env;
+use std::io::{Error, ErrorKind};
+use url::Url;
 
 // All ops are registered here
-pub fn deno_plugin_init(interface: &mut dyn Interface) {
-    interface.register_op("connect", op_connect);
+pub fn init(i: &mut CoreIsolate, s: &State) {
+    i.register_op("connect", s.stateful_json_op(op_connect));
+}
+
+#[derive(Deserialize, Debug)]
+struct ConnectArgs {
+    addr: SocketAddr,
+}
+
+fn op_connect(
+    state: &State,
+    args: Value,
+    _zero_copy: &mut [ZeroCopyBuf],
+) -> Result<JsonOp, OpError> {
+    let args: ConnectArgs = serde_json::from_value(args)?;
+    dbg!(args);
+    let v = FtpStream::connect(args.addr);
+    Ok(JsonOp::Sync(r))
 }
 
 #[cfg(test)]
